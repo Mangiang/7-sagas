4a9067c89cadd690bb3bb2c54e3563545763679f
Mangiang <mangiang@orange.fr>
2018-12-21 23:01:44 +0100

    Change settings times to seconds instead of milliseconds

--- src/layouts/SettingsMenu.js
+++ src/layouts/SettingsMenu.js
@@ -9,10 +9,10 @@ const mapStateToProps = state => ({
 });
 
 const handleTimeIntervalChange = (dispatchObject, event) => {
-    dispatchObject.dispatch({type: 'GAME_CHANGE_TIME_INTERVAL_REQUESTED', newTimeInterval: parseInt(event.target.value)});
+    dispatchObject.dispatch({type: 'GAME_CHANGE_TIME_INTERVAL_REQUESTED', newTimeInterval: parseInt(event.target.value*1000)});
 };
 const handleSpawnIntervalChange = (dispatchObject, event) => {
-    dispatchObject.dispatch({type: 'GAME_CHANGE_SPAWN_INTERVAL_REQUESTED', newSpawnInterval: parseInt(event.target.value)});
+    dispatchObject.dispatch({type: 'GAME_CHANGE_SPAWN_INTERVAL_REQUESTED', newSpawnInterval: parseInt(event.target.value*1000)});
 };
 const handleLivesCountChange = (dispatchObject, event) => {
     dispatchObject.dispatch({type: 'GAME_CHANGE_LIVES_COUNT_REQUESTED', newLivesCount: parseInt(event.target.value)});
@@ -39,13 +39,15 @@ const SettingsMenu = ({TIME_INTERVAL, SPAWN_INTERVAL, lives, dispatch}) => (
         </div>
         <div style={{marginBottom: '10px'}}>
             <label htmlFor="timeIntervalInput">Time Interval : </label>
-            <input id="timeIntervalInput" type="number" min={100} max={10000} onInput={handleTimeIntervalChange.bind(this, {dispatch: dispatch})} defaultValue={TIME_INTERVAL}/>
+            <input style={{width:'50px'}} id="timeIntervalInput" type="number" min={0.01} max={10} onInput={handleTimeIntervalChange.bind(this, {dispatch: dispatch})} defaultValue={TIME_INTERVAL/1000}/>
+            <label> seconds.</label>
         </div>
         <div style={{marginBottom: '10px'}}>
             <label htmlFor="spawnIntervalInput">Spawn Interval : </label>
-            <input id="spawnIntervalInput" type="number" min={100} max={10000} onInput={handleSpawnIntervalChange.bind(this, {dispatch: dispatch})} defaultValue={SPAWN_INTERVAL}/>
+            <input style={{width:'50px'}} id="spawnIntervalInput" type="number" min={0.01} max={10} onInput={handleSpawnIntervalChange.bind(this, {dispatch: dispatch})} defaultValue={SPAWN_INTERVAL/1000}/>
+            <label> seconds.</label>
         </div>
-        <ButtonQuit onClick={() => dispatch({type: 'GAME_MAIN_MENU_REQUESTED'})}/>
+        <ButtonQuit style={{marginUp: '30px'}} onClick={() => dispatch({type: 'GAME_MAIN_MENU_REQUESTED'})}/>
     </div>
 );
 
